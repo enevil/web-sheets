fc4e0136b7f0abd3c089fa8ee05e96e5b1df0c24
enevil <enevilx@gmail.com>
2022-04-05 17:41:18 +0300

    react router correct get index

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ updateDb.start();
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "./client/build")));
-  app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-  });
+  // app.get("*", function (req, res) {
+  //   res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  // });
 }
